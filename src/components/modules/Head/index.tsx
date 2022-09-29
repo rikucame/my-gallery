d5a6ce19f5de1409ab8a9248fbb27cc7b702279f
rikucame <rikucame@gmail.com>
2022-09-30 02:32:44 +0900

    SEO対策

--- src/components/modules/Head/index.tsx
+++ src/components/modules/Head/index.tsx
@@ -3,17 +3,30 @@ import { memo } from "react";
 import { Helmet } from "react-helmet";
 import { CreateGlobalStyle } from "../../../GrobalStyle";
 
+const url = "https://rikutoishikura.com";
+const title = "Rikuto Ishikura";
+const description = "Rikuto Ishikura | Rikkun | Photographer";
+const faviconPath = "/favicon.svg";
+
+const OgpSettings: React.VFC = () => (
+  <>
+    <meta property="og:url" content={url} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={`${url}/ogp.jpg`} />
+  </>
+);
+
 export const Head: React.VFC = memo(() => {
   return (
     <>
-      <Helmet
-        title="Rikuto Ishikura"
-        link={[
-          { lang: "ja" },
-          { rel: "stylesheet", href: "https://use.typekit.net/lpr1vrm.css" },
-          { rel: "icon", type: "image/svg", href: "/icon.png" },
-        ]}
-      />
+      <Helmet>
+        <title>{title}</title>
+        <link rel="shortcut icon" href={faviconPath} type="image/svg" />
+        <link rel="stylesheet" href="https://use.typekit.net/lpr1vrm.css" />
+        <meta name="description" content={description} />
+        <OgpSettings />
+      </Helmet>
       <CreateGlobalStyle />
     </>
   );
